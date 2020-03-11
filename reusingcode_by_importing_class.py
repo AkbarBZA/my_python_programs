fc061f09946873f566fecffdedf8c202bfc7af05
AkbarBZA <61173385+AkbarBZA@users.noreply.github.com>
2020-03-10 23:35:27 -0400

    Add files via upload

---
+++ reusingcode_by_importing_class.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import testclass as tc
+
+
+# In[6]:
+
+
+nobj=tc.Animal('Tommy',6)
+
+
+# In[3]:
+
+
+nobj.sit()
+
+
+# In[4]:
+
+
+nobj.roll_over()
+
+
+# In[ ]:
+
+
+
+
